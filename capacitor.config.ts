b8b1377c08144fb9c780099143589284bdcdc069
noviarn <42713448+noviarn@users.noreply.github.com>
2021-10-10 21:35:42 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'uts_mobile2_25471',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
